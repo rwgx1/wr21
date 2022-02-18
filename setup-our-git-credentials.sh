43efb5dc831e2b7c9b90a49a7904b5d0bb916c8a
Richard <rwgx57@gmail.com>
2022-02-18 22:51:29 +0000

    minor update to setup-our-git-credentials.sh

--- setup-our-git-credentials.sh
+++ setup-our-git-credentials.sh
@@ -82,7 +82,7 @@ case $CHOICE in
 			git config --global pull.rebase false
 			git config --global push.default simple
 			git config --global user.name "Richard "
-			git config --global user.email "rwgx57@gmail.com"
+			### git config --global user.email "xxxxx@gmail.com"
 			sudo git config --system core.editor nano
 			git config --global credential.helper cache
 			git config --global credential.helper 'cache --timeout=32000'
